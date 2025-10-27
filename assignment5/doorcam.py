fa89a489a7372b1c4a230a18cde202ea0eca77db
berke <daw.berkeij@student.han.nl>
2025-10-27 10:12:14 +0100

    did the small change

--- assignment5/doorcam.py
+++ assignment5/doorcam.py
@@ -131,7 +131,7 @@ def main_loop():
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
+        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
 
         # Find all the face locations and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
